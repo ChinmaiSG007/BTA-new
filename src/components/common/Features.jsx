d4300abd54a247c8b3cb1102624e0defefcdd5b4
ChinmaiSG007 <chinmaicsg8787@gmail.com>
2025-03-10 23:14:16 +0530

    about and count

--- src/components/common/Features.jsx
+++ src/components/common/Features.jsx
@@ -2,6 +2,10 @@ import { useState, useRef } from "react";
 import { TiLocationArrow } from "react-icons/ti";
 import ScrollVelocity from "./../styling/ScrollerVelocity"
 import TiltedCard from "../styling/TiltedCard";
+import SpotlightCard from "../styling/SpotlightCard";
+import CountUp from "../styling/CountUp";
+
+
 export const BentoTilt = ({ children, className = "" }) => {
   const [transformStyle, setTransformStyle] = useState("");
   const itemRef = useRef(null);
@@ -216,6 +220,51 @@ const Features = () => (
         <div className="p-4">
           <h1 className="text-2xl font-bold mb-4">Meet your Guide: Palaksha</h1>
           <p className=" text-justify">Palaksha fondly known as <strong>"Pali"</strong> is the mad hatter of the pack, motorcycle collector, off-road enthusiast & dirt track guy. He is one of those guys who chose motorcycling as a part of his life and made it his profession. Owning, Riding and Working on his 20 odd motorcycles (albeit few rare ones) alone is testament to his passion for motorcycles. Pali is WAFA Certified and has a rich experience in leading 35 motorcycle tours pan India. His quick wit and perspicacious nature and impeccable decision making has ensured successful completion of all the tours led by him. Our lead road captain's knowledge of Geographies and Motorcycles is immense and it shows in the stories experienced and shared by our clients. He is insatiably curious, open minded and goes beyond the call of duty to get things done!</p>
+        </div>
+      </div>
+
+      <div className="bg-white flex gap-5 p-4 my-4 rounded">
+        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.5)">
+          <img src="img/images/16.svg" alt="" />
+        </SpotlightCard>
+        <div className="p-4">
+          <h1 className="text-2xl font-bold mb-4">Unleashing the Spirit of Adventure: Beyond Tarmac's Guided Motorcycle Tours</h1>
+          <p className=" text-justify">Beyond Tarmac was born from the fervent dedication of a collective of motorcycle aficionados, united by an unbridled passion for the art of motorcycling. As trailblazers in the South Indian motorcycle scene, we have established a formidable presence nationwide, distinguishing ourselves as pioneers in the industry. For over six years, we have orchestrated meticulously curated guided motorcycle tours, each journey meticulously crafted to deliver unforgettable experiences on the open road. With a track record of over 780+ delighted clients, our commitment to excellence and unwavering dedication to our craft continue to propel us forward, shaping the landscape of motorcycle touring in India.</p>
+        </div>
+      </div>
+
+      <div className="text-white flex justify-between">
+        <div>
+          <CountUp
+            from={0}
+            to={83310}
+            separator=","
+            direction="up"
+            duration={1}
+            className="count-up-text text-4xl font-bold" />
+          <p>Kilometers done</p>
+        </div>
+        <div>
+          <CountUp
+            from={0}
+            to={780}
+            separator=","
+            direction="up"
+            duration={1}
+            className="count-up-text text-4xl font-bold" />
+          <p>Clients
+            Served</p>
+        </div>
+        <div>
+          <CountUp
+            from={0}
+            to={330}
+            separator=","
+            direction="up"
+            duration={1}
+            className="count-up-text text-4xl font-bold" />
+          <p>Women
+            Trained</p>
         </div>
       </div>
     </div>
