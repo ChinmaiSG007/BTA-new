451899022d96a33c0831031f15e944dcd7eea6cf
ChinmaiSG007 <chinmaicsg8787@gmail.com>
2025-03-20 23:19:01 +0530

    Gallery

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,6 +4,7 @@ import Features from "../common/Features";
 import Story from "../common/Story";
 import Contact from "../common/Contact";
 import CircularGallery from "../styling/CircularGallery";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
@@ -11,10 +12,14 @@ const Home = () => {
             <Hero />
             <About />
             <Features />
-            <div style={{ height: '600px', position: 'relative' }}>
+            <div style={{ height: '800px', position: 'relative', marginBottom: '16rem' }}>
+                <h3 className='section-heading'>Gallery</h3>
                 <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} />
+                <Link to={'/gallery'} className='flex flex-col justify-center items-center gap-4'>
+                    <button className='action-button'>See more</button>
+                </Link>
             </div>
-            <Story />
+            {/* <Story /> */}
             <Contact />
         </div>
     )
--- src/components/styling/CircularGallery.jsx
+++ src/components/styling/CircularGallery.jsx
@@ -11,6 +11,7 @@ import {
 
 import './CircularGallery.css';
 
+
 function debounce(func, wait) {
     let timeout
     return function (...args) {
@@ -330,18 +331,12 @@ class App {
     }
     createMedias(items, bend = 1, textColor, borderRadius, font) {
         const defaultItems = [
-            { image: `https://picsum.photos/seed/1/800/600?grayscale`, text: 'Bridge' },
-            { image: `https://picsum.photos/seed/2/800/600?grayscale`, text: 'Desk Setup' },
-            { image: `https://picsum.photos/seed/3/800/600?grayscale`, text: 'Waterfall' },
-            { image: `https://picsum.photos/seed/4/800/600?grayscale`, text: 'Strawberries' },
-            { image: `https://picsum.photos/seed/5/800/600?grayscale`, text: 'Deep Diving' },
-            { image: `https://picsum.photos/seed/16/800/600?grayscale`, text: 'Train Track' },
-            { image: `https://picsum.photos/seed/17/800/600?grayscale`, text: 'Santorini' },
-            { image: `https://picsum.photos/seed/8/800/600?grayscale`, text: 'Blurry Lights' },
-            { image: `https://picsum.photos/seed/9/800/600?grayscale`, text: 'New York' },
-            { image: `https://picsum.photos/seed/10/800/600?grayscale`, text: 'Good Boy' },
-            { image: `https://picsum.photos/seed/21/800/600?grayscale`, text: 'Coastline' },
-            { image: `https://picsum.photos/seed/12/800/600?grayscale`, text: "Palm Trees" }
+            { image: 'img/images/gallery/compressedImages/DSC_5469.jpg', text: '' },
+            { image: 'img/images/gallery/compressedImages/DSC_5007.jpg', text: '' },
+            { image: 'img/images/gallery/compressedImages/DSC_5025.jpg', text: '' },
+            { image: 'img/images/gallery/compressedImages/DSC_5315.jpg', text: '' },
+            { image: 'img/images/gallery/compressedImages/SMR_6773.jpg', text: '' },
+            { image: 'img/images/gallery/compressedImages/SMR_9680.jpg', text: '' },
         ]
         const galleryItems = items && items.length ? items : defaultItems
         this.mediasImages = galleryItems.concat(galleryItems)
